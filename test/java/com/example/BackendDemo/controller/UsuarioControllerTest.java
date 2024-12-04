ae0f528d86b1b9cda898e7471f0fb9bdafb98c59
Andres Bedoya <167808400+andybd2703@users.noreply.github.com>
2024-12-03 23:18:34 -0500

    Add files via upload

---
+++ test/java/com/example/BackendDemo/controller/UsuarioControllerTest.java
@@ -0,0 +1,40 @@
+package com.example.BackendDemo.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class UsuarioControllerTest {
+    @Test
+    void testGuardarUsuario() {
+
+    }
+
+    @Test
+    void testListarUsuarios() {
+
+    }
+
+    @Test
+    void testMostrarFormulario() {
+
+    }
+
+    @Test
+    void testGuardarUsuario2() {
+        
+    }
+
+    @Test
+    void testGuardarUsuarioAPI() {
+        
+    }
+
+    @Test
+    void testListarUsuarios2() {
+        
+    }
+
+    @Test
+    void testMostrarFormulario2() {
+        
+    }
+}
